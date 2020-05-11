bf4cc7f8547a9651d11578be14ae58953053d0b7
Jing Yeh <jing.ye@timecitytech.com>
2020-05-11 21:24:41 +0800

    影音紀錄-引言

--- src/components/videoRecord/video_child/video_preface3.vue
+++ src/components/videoRecord/video_child/video_preface3.vue
@@ -3,27 +3,40 @@
     <div class="title">
       <h2>流行音樂</h2>
       <span @click="close" class="cross">
-        <img width="20" src="./../../../assets/cross.png">
+        <img width="20" src="./../../../assets/cross.png" />
       </span>
     </div>
     <!-- <hr> -->
     <p class="firstRow"></p>
-    <p>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;有鑑於本年鑑所記載、論述的內容主要為2018年度臺灣音樂活動，影音紀錄更能幫助民眾理解以音樂為主體的各項音樂動態，《2018臺灣音樂年鑑》工作團隊擇選具有代表性的音樂活動影像，以做為《2018臺灣音樂年鑑》的保存與補充資料。本年鑑共收錄51支臺灣音樂影音紀錄，其中流行音樂篇收錄2支影片。
-    </p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;有鑑於本年鑑所記載的內容主要為2019年度臺灣音樂活動，影音紀錄更能幫助民眾理解以音樂為主體的各項音樂動態，《2019臺灣音樂年鑑》工作團隊擇選具有代表性的音樂活動影像，以做為《2019臺灣音樂年鑑》的保存與補充資料。本年鑑共收錄51支臺灣音樂影音紀錄，其中流行音樂篇收錄1支影片。</p>
+    <table rules="all" style="border:1px solid" cellpadding="5">
+      <tbody>
+        <tr>
+          <td>
+            流行音樂
+          </td>
+          <td>
+            13_2
+          </td>
+          <td>
+            桃園鐵玫瑰音樂節鐵粉搖滾趴
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      show: true,
+      show: true
     };
   },
   methods: {
     close() {
       this.show = false;
-    },
-  },
+    }
+  }
 };
 </script>
--- src/components/videoRecord/video_child/video_preface4.vue
+++ src/components/videoRecord/video_child/video_preface4.vue
@@ -3,27 +3,56 @@
     <div class="title">
       <h2>跨界音樂</h2>
       <span @click="close" class="cross">
-        <img width="20" src="./../../../assets/cross.png">
+        <img width="20" src="./../../../assets/cross.png" />
       </span>
     </div>
     <!-- <hr> -->
     <p class="firstRow"></p>
-    <p>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;有鑑於本年鑑所記載、論述的內容為2018年度臺灣音樂活動，影音紀錄更能幫助民眾理解以音樂為主體的各項活動實況，《2018臺灣音樂年鑑》工作團隊擇選具有代表性的音樂活動影像，以做為《2018臺灣音樂年鑑》的保存與補充資料。本年鑑共收錄51支臺灣音樂影音紀錄，其中跨界音樂篇收錄1支影片。
-    </p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;有鑑於本年鑑所記載的內容為2019年度臺灣音樂活動，影音紀錄更能幫助民眾理解以音樂為主體的各項活動實況，《2019臺灣音樂年鑑》工作團隊擇選具有代表性的音樂活動影像，以做為《2019臺灣音樂年鑑》的保存與補充資料。本年鑑共收錄51支臺灣音樂影音紀錄，其中跨界音樂其他篇收錄3支影片。</p>
+    <table rules="all" style="border:1px solid" cellpadding="5">
+      <tbody>
+        <tr>
+          <td rowspan="3">
+            跨界音樂與其他
+          </td>
+          <td>
+            14_1
+          </td>
+          <td>
+            臺灣燈會在屏東－國際燈區新住民藝術表演「藝遊未境」系列活動
+          </td>
+        </tr>
+        <tr>
+          <td>
+            14_2
+          </td>
+          <td>
+            東南亞樂舞表演
+          </td>
+        </tr>
+        <tr>
+          <td>
+            14_3
+          </td>
+          <td>
+            爪哇伯公：漫步日惹鄉村傳唱臺灣民間故事
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      show: true,
+      show: true
     };
   },
   methods: {
     close() {
       this.show = false;
-    },
-  },
+    }
+  }
 };
 </script>
